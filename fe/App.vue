3097d6c093c9367215bab4664e51fe33dd8d2867
hongming li <hongmingli1988@gmail.com>
2017-03-13 21:22:15 -0700

    Thanks God. Vue-router works now.

--- fe/App.vue
+++ fe/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     <h1></h1>
     <h2>Essential Links</h2>
     <ul>
--- fe/main.js
+++ fe/main.js
@@ -3,13 +3,17 @@ import App from './App.vue'
 import About from './About.vue'
 import VueRouter from 'vue-router'
 
+Vue.use(VueRouter)
+
 const routes = [
   { path: '/', component: App },
   { path: '/about', component: About }
 ];
 
 const router = new VueRouter({
-  routes
+  routes: routes,
+  // https://router.vuejs.org/en/essentials/history-mode.html
+  mode: 'history' // If you want use hash in the url, remove this line.
 });
 
 new Vue({
