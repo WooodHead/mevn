e964e097480db3f6d47cdbaed3b6f707a77cf047
lihongming <hongmingli1988@gmail.com>
2017-03-13 17:39:29 -0700

    add vue-router, not test yet. God bless it.

--- fe/main.js
+++ fe/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import About from './About.vue'
+import VueRouter from 'vue-router'
+
+const routes = [
+  { path: '/', component: App },
+  { path: '/about', component: About }
+];
+
+const router = new VueRouter({
+  routes
+});
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  router
+}).$mount('#app');
